91caee4bc363a27e127afb03a6e49b57125e9887
JaceyKan <jacey_kan@sina.com>
2018-09-28 22:13:11 +0800

    issues problem

---
+++ comment.rb
@@ -0,0 +1,28 @@
+username = "jaceykan" # GitHub 用户名
+new_token = "fee15b8168daf36b7457e804628b2126baec81fd"  # GitHub Token
+repo_name = "jaceykan.gituhb.io" # 存放 issues
+sitemap_url = "https://jaceykan.github.io/sitemap.xml" # sitemap
+kind = "Gitalk" # "Gitalk" or "gitment"
+
+require 'open-uri'
+require 'faraday'
+require 'active_support'
+require 'active_support/core_ext'
+require 'sitemap-parser'
+
+sitemap = SitemapParser.new sitemap_url
+urls = sitemap.to_a
+
+conn = Faraday.new(:url => "https://api.github.com/repos/#{username}/#{repo_name}/issues") do |conn|
+  conn.basic_auth(username, token)
+  conn.adapter  Faraday.default_adapter
+end
+
+urls.each_with_index do |url, index|
+  title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
+  response = conn.post do |req|
+    req.body = { body: url, labels: [kind, url], title: title }.to_json
+  end
+  puts response.body
+  sleep 15 if index % 20 == 0
+end
